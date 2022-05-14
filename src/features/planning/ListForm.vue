51e2853bc3f25db31e1416b81f4fd3129ac4d2d5
Adrian Dąbek <adrianda@backbase.com>
2022-05-14 13:34:03 +0200

    add new lists

---
+++ src/features/planning/ListForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input type="text" v-model="name" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Emits {
+  (e: 'newList', name: string): void;
+}
+const emits = defineEmits<Emits>();
+
+let name = ref('');
+
+function onSubmit() {
+  emits('newList', name.value);
+  name.value = '';
+}
+</script>
--- src/features/planning/ListsContainer.vue
+++ src/features/planning/ListsContainer.vue
@@ -5,16 +5,21 @@
     :key="list"
     @remove="onRemove(list)"
   ></ListComponent>
+  <ListForm @newList="onNewList($event)"></ListForm>
 </template>
 
 <script setup lang="ts">
 import ListComponent from '@/features/planning/ListComponent.vue';
 import { ref } from 'vue';
+import ListForm from '@/features/planning/ListForm.vue';
 
 let lists = ref(['List1', 'List2', 'List3']);
 
 function onRemove(name: string) {
-  console.log('REMOVAL', name);
   lists.value = lists.value.filter((n) => n !== name);
+}
+
+function onNewList(name: string) {
+  lists.value = [...lists.value, name];
 }
 </script>
