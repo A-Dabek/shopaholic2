19e9ce35d83c9a021eca88e5146eacb59c409280
Adrian Dąbek <adrianda@backbase.com>
2022-05-14 21:51:54 +0200

    introduce planning store

--- src/features/planning/ListsContainer.vue
+++ src/features/planning/ListsContainer.vue
@@ -1,121 +1,24 @@
 <template>
   <div class="block">
-    <ListForm @newList="onNewList"></ListForm>
+    <ListForm @newList="store.addList"></ListForm>
   </div>
   <ListComponent
-    v-for="list in lists"
+    v-for="list in store.lists"
     :list="list"
     :key="list.name"
-    @remove="onRemove(list.name)"
-    @clear="onClear(list.name)"
-    @newItem="onNewItem(list.name, $event)"
-    @increment="onIncrement(list.name, $event)"
-    @decrement="onDecrement(list.name, $event)"
-    @removeItem="onRemoveItem(list.name, $event)"
+    @remove="store.removeList(list.name)"
+    @clear="store.clearList(list.name)"
+    @newItem="store.addItemToList(list.name, $event)"
+    @increment="store.incrementItemQuantity(list.name, $event)"
+    @decrement="store.decrementItemQuantity(list.name, $event)"
+    @removeItem="store.removeItemFromList(list.name, $event)"
   ></ListComponent>
 </template>
 
 <script setup lang="ts">
 import ListComponent from '@/features/planning/ListComponent.vue';
-import { ref } from 'vue';
 import ListForm from '@/features/planning/ListForm.vue';
-import type { ShoppingItem, ShoppingList } from '@/features/planning/types';
+import { usePlanningStore } from '@/stores/planning.store';
 
-let lists = ref<ShoppingList[]>([
-  {
-    name: 'List1',
-    items: [
-      {
-        name: 'item1',
-        description: 'desc rip tion 1',
-        quantity: 23,
-        quantityType: 'kg',
-        timestamp: new Date().getTime(),
-        urgent: false,
-      },
-      {
-        name: 'item2',
-        description: 'desc rip tion 1',
-        quantity: 23,
-        quantityType: 'kg',
-        timestamp: new Date().getTime(),
-        urgent: false,
-      },
-      {
-        name: 'item3',
-        description: 'desc rip tion 1',
-        quantity: 23,
-        quantityType: 'kg',
-        timestamp: new Date().getTime(),
-        urgent: false,
-      },
-      {
-        name: 'item4',
-        description: 'desc rip tion 1',
-        quantity: 23,
-        quantityType: 'kg',
-        timestamp: new Date().getTime(),
-        urgent: false,
-      },
-    ],
-  },
-  {
-    name: 'List2',
-    items: [],
-  },
-  {
-    name: 'List3',
-    items: [],
-  },
-]);
-
-function onNewList(name: string) {
-  lists.value = [...lists.value, { name: name, items: [] }];
-}
-
-function onNewItem(name: string, item: ShoppingItem) {
-  lists.value = lists.value.map(list => {
-    if (list.name !== name) return list;
-    return { ...list, items: [...list.items, item] };
-  });
-}
-
-function onClear(name: string) {
-  lists.value = lists.value.map(list => {
-    if (list.name !== name) return list;
-    return { ...list, items: [] };
-  });
-}
-
-function onRemove(name: string) {
-  lists.value = lists.value.filter(list => list.name !== name);
-}
-
-function onIncrement(list: string, item: string) {
-  lists.value = lists.value.map(l => {
-    if (l.name !== list) return l;
-    const items = l.items.map(i => {
-      if (i.name !== item) return i;
-      return { ...i, quantity: i.quantity + 1 };
-    });
-    return { ...l, items };
-  });
-}
-function onDecrement(list: string, item: string) {
-  lists.value = lists.value.map(l => {
-    if (l.name !== list) return l;
-    const items = l.items.map(i => {
-      if (i.name !== item) return i;
-      return { ...i, quantity: i.quantity - 1 };
-    });
-    return { ...l, items };
-  });
-}
-function onRemoveItem(list: string, item: string) {
-  lists.value = lists.value.map(l => {
-    if (l.name !== list) return l;
-    const items = l.items.filter(i => i.name !== item);
-    return { ...l, items };
-  });
-}
+let store = usePlanningStore();
 </script>
---
+++ src/stores/planning.store.ts
@@ -0,0 +1,81 @@
+import { defineStore } from 'pinia';
+import type { ShoppingItem, ShoppingList } from '@/features/planning/types';
+
+function modifyList(
+  lists: ShoppingList[],
+  listName: string,
+  mapper: (list: ShoppingList) => ShoppingList
+) {
+  return lists.map(list => {
+    if (list.name !== listName) return list;
+    return mapper(list);
+  });
+}
+
+function modifyItem(
+  items: ShoppingItem[],
+  itemName: string,
+  mapper: (item: ShoppingItem) => ShoppingItem
+) {
+  return items.map(item => {
+    if (item.name !== itemName) return item;
+    return mapper(item);
+  });
+}
+
+export const usePlanningStore = defineStore({
+  id: 'planning',
+  state: () => ({
+    lists: [
+      {
+        name: 'List2',
+        items: [],
+      },
+    ] as ShoppingList[],
+  }),
+  getters: {},
+  actions: {
+    addList(name: string) {
+      this.lists = [...this.lists, { name: name, items: [] }];
+    },
+    removeList(name: string) {
+      this.lists = this.lists.filter(list => list.name !== name);
+    },
+    clearList(name: string) {
+      this.lists = modifyList(this.lists, name, list => ({
+        ...list,
+        items: [],
+      }));
+    },
+    addItemToList(listName: string, item: ShoppingItem) {
+      this.lists = modifyList(this.lists, listName, list => ({
+        ...list,
+        items: [...list.items, item],
+      }));
+    },
+    removeItemFromList(listName: string, itemName: string) {
+      this.lists = modifyList(this.lists, listName, list => ({
+        ...list,
+        items: list.items.filter(item => item.name !== itemName),
+      }));
+    },
+    incrementItemQuantity(listName: string, itemName: string) {
+      this.lists = modifyList(this.lists, listName, list => ({
+        ...list,
+        items: modifyItem(list.items, itemName, item => ({
+          ...item,
+          quantity: item.quantity + 1,
+        })),
+      }));
+    },
+    decrementItemQuantity(listName: string, itemName: string) {
+      this.lists = modifyList(this.lists, listName, list => ({
+        ...list,
+        items: modifyItem(list.items, itemName, item => ({
+          ...item,
+          quantity: item.quantity - 1,
+        })),
+      }));
+    },
+  },
+});
