b25ce526cbfcc705d6ef0cd593a59b2a1a25287d
Adrian Dąbek <adrianda@backbase.com>
2022-05-14 14:58:38 +0200

    remove original home page

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,6 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/planning">Planning</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import PlanningView from '../features/planning/PlanningView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/planning',
       name: 'planning',
-      component: () => import('../features/planning/PlanningView.vue'),
+      component: PlanningView,
     },
   ],
 });
