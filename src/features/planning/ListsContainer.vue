8a9d214ab429f032ee7092167123d4a1091c3944
Adrian Dąbek <adrianda@backbase.com>
2022-05-14 13:14:01 +0200

    display lists

---
+++ src/features/planning/ListsContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <ListComponent v-for="list in lists" :name="list" :key="list"></ListComponent>
+</template>
+
+<script setup lang="ts">
+import ListComponent from '@/features/planning/ListComponent.vue';
+import { reactive } from 'vue';
+
+const lists = reactive(['List1', 'List2', 'List3']);
+</script>
---
+++ src/features/planning/PlanningView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ListsContainer from '@/features/planning/ListsContainer.vue';
+</script>
+
+<template>
+  <main>
+    <ListsContainer />
+  </main>
+</template>
