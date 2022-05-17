e29dc24e10057d3d8b286615009461ff8a195077
Adrian Dąbek <adrianda@backbase.com>
2022-05-17 15:30:26 +0200

    listen to bought items changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
   <main class="container is-max-widescreen p-1">
     <RouterView />
   </main>
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import { watchEffect } from 'vue';
+import { cloudStore } from '@/stores/firebase.store';
+import { usePlanningStore } from '@/stores/planning.store';
+
+const store = usePlanningStore();
+watchEffect(async () => {
+  return cloudStore.onBoughtSnapshot(items => store.setBoughtItems(items));
+});
+</script>
 
 <style>
 @import '../node_modules/bulma/css/bulma.css';
